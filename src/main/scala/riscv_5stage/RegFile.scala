b0be3cab1b7911b0e185577bd156e33a5fffdbcf
rayjyh <raymondjyh@gmail.com>
2020-03-18 22:37:53 +0800

    RegFile Tests

--- src/main/scala/riscv_5stage/RegFile.scala
+++ src/main/scala/riscv_5stage/RegFile.scala
@@ -3,18 +3,18 @@ package riscv_5stage
 import chisel3._
 import chisel3.util._
 
-class RegFileIO(RegAddrWidth: Int, DataWidth: Int)  extends Bundle {
-  val rs1       = Input(UInt(RegAddrWidth.W))
-  val rs2       = Input(UInt(RegAddrWidth.W))
+class RegFileIO(DataWidth: Int)  extends Bundle {
+  val rs1       = Input(UInt(5.W))
+  val rs2       = Input(UInt(5.W))
   val rs1_data  = Output(UInt(DataWidth.W))
   val rs2_data  = Output(UInt(DataWidth.W))
   val RegWrite  = Input(Bool())
-  val WriteReg  = Input(UInt(RegAddrWidth.W))
+  val WriteReg  = Input(UInt(5.W))
   val WriteData = Input(UInt(DataWidth.W))
 }
 
-class RegFile() extends Module with CoreParams {
-  val io = IO(new RegFileIO)
+class RegFile(DataWidth: Int) extends Module {
+  val io = IO(new RegFileIO(DataWidth))
   val regs = Mem(32, UInt(DataWidth.W))
   io.rs1_data := Mux(io.rs1.orR, regs(io.rs1), 0.U)
   io.rs2_data := Mux(io.rs2.orR, regs(io.rs2), 0.U)
